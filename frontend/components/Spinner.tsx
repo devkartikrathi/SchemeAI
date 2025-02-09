9e800557b243be6d3af13f847f8281007059ed0b
Piyush Sharma <piyushkumar40515s@gmail.com>
2025-02-09 13:57:31 +0530

    initial commit

--- frontend/components/Spinner.tsx
+++ frontend/components/Spinner.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className="flex justify-center items-center py-4">
+        <div className="flex justify-center items-center py-4 bg-gray-100">
             <div className="relative w-6 h-6">
                 <div className="absolute border-2 border-gray-700 opacity-50 rounded-full animate-ping w-full h-full"></div>
                 <div className="absolute border-2 border-gray-500 opacity-40 rounded-full animate-ping w-full h-full delay-150"></div>
